6529996dd6f85a86d35c7fb403742a5d07e4e160
camargodev <camargo.dev@gmail.com>
2020-09-27 15:35:33 -0300

    1475 Approved

--- 1475/1475.cpp
+++ 1475/1475.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 #include <map>
 
 using namespace std; 
@@ -14,27 +15,35 @@ ull L, P1, P2;
 //     return find(positions.begin(), positions.end(), pos) != positions.end();
 // }
 
-ull minVal(ull pos, vector<bool> covered) {
+ull minVal(ull pos, vector<ull> holesLeft) {
     
     // pair<ull, vector<bool>> pairIndex = make_pair(pos, covered);
     ull index = pos;
 
+    // cout << "HERE 1" << endl;
     if (minValues.find(index) != minValues.end()) {
         // cout << "Put " << minValues[index] << " for " << pos << endl;
         return minValues[index];
     }
 
+    // cout << "HERE 2" << endl;
     if (pos >= L-1) { 
         // cout << "End: " << pos << endl;
         return 0;
     }
-
-    if (covered[pos]) { 
-        int next = pos+1;
-        while (covered[next] && next<L) next+=1;
+    // cout << "HERE 3 " << index  << endl;
+// find(v.begin(), v.end(), key) != v.end()
+    if (find(holesLeft.begin(), holesLeft.end(), index) == holesLeft.end()) { 
+        // int next = pos+1;
+        // while (covered[next] && next<L) next+=1;
+        if (holesLeft.size() == 0) {
+            // cout << "No hole left. Going Back. " << endl;
+            return 0;
+        }
+        int next = holesLeft[0];
         // int next = nexts[pos];
         // cout << "Skipping " << pos << " > going to " << next << endl;
-        return minVal(next, covered);
+        return minVal(next, holesLeft);
     }
 
     // cout << "AT: " << pos << endl;
@@ -43,33 +52,40 @@ ull minVal(ull pos, vector<bool> covered) {
     // for (ull i = 0; i < L; i++) cout << covered[i] << " ";
     // cout << "]" << endl;
 
-    vector<bool> coveredP1 = covered;
+    // cout << "HERE 4" << endl;
+    vector<ull> holesLeftWithP1 = holesLeft;
     ull nextWithP1 = pos+P1+1;
-    for (ull i = pos; i < nextWithP1; i++)
-        coveredP1[i] = true;
+    while (holesLeftWithP1.size()>0 && holesLeftWithP1[0] < nextWithP1)
+        holesLeftWithP1.erase(holesLeftWithP1.begin());
+    // for (ull i = pos; i < nextWithP1; i++)
+    //     coveredP1[i] = true;
     // cout << "AT: " << pos << ": Sum " << P1 << " > going to " << nextWithP1 << endl;
-    ull valWithP1 = P1 + minVal(nextWithP1, coveredP1);
+    ull valWithP1 = P1 + minVal(nextWithP1, holesLeftWithP1);
 
-    vector<bool> coveredP2 = covered;
+    // vector<bool> coveredP2 = covered;
+    // ull nextWithP2 = pos+P2+1;
+    // for (ull i = pos; i < nextWithP2; i++)
+    //     coveredP2[i] = true;
+    vector<ull> holesLeftWithP2 = holesLeft;
     ull nextWithP2 = pos+P2+1;
-    for (ull i = pos; i < nextWithP2; i++)
-        coveredP2[i] = true;
     // cout << "AT: " << pos << ": Sum " << P2 << " > going to " << nextWithP2 << endl;
-    ull valWithP2 = P2 + minVal(nextWithP2, coveredP2);
+    while (holesLeftWithP2.size()>0 && holesLeftWithP2[0] < nextWithP2)
+        holesLeftWithP2.erase(holesLeftWithP2.begin());
+    ull valWithP2 = P2 + minVal(nextWithP2, holesLeftWithP2);
 
     // return min(valWithP1, valWithP2);
-    ull bestVal;
-    if (valWithP1 <= valWithP2) {
-        // pairIndex = make_pair(pos, coveredP1);
-        bestVal = valWithP1;
-    } else {
-        // pairIndex = make_pair(pos, coveredP2);
-        bestVal = valWithP2;
-    }
+    // ull bestVal;
+    // if (valWithP1 <= valWithP2) {
+    //     // pairIndex = make_pair(pos, coveredP1);
+    //     bestVal = valWithP1;
+    // } else {
+    //     // pairIndex = make_pair(pos, coveredP2);
+    //     bestVal = valWithP2;
+    // }
 
-    minValues[index] = bestVal;
-    // cout << "Caching " << bestVal << " for " << pos << endl;
-    return bestVal;
+    minValues[index] = min(valWithP1, valWithP2);
+    // cout << "Caching " << minValues[index] << " for " << pos << endl;
+    return minValues[index];
 }
 
 // ull calculateMinVal(vector<bool> covered) {
@@ -95,14 +111,16 @@ int main() {
         // ull totalLength, patch1, patch2;
         cin >> L >> P1 >> P2;
         // cout << holes << " " << L << " " << P1 << " " << P2 << endl;
-        vector<bool> covered(L, true);
+        // vector<bool> covered(L, true);
+        vector<ull> holesLeft;
         // for(int i=0;i<L;i++) nexts.push_back(0);
         // bool isFirst = true; ull first;
         for (ull i = 0; i < holes; i++) {
             ull pos; cin >> pos;
             // cout << "Hole at " << pos-1 << endl;
             // positions.push_back(pos);
-            covered[pos-1] = false;
+            // covered[pos-1] = false;
+            holesLeft.push_back(pos-1);
             // if (isFirst) { first = pos-1; isFirst = false; }
         }
         // int currentNext = L-1;
@@ -114,7 +132,7 @@ int main() {
         //     nexts[j-1] = currentNext;
         // }
         // printTables(covered,nexts);
-        ull res = minVal(0, covered);
+        ull res = minVal(0, holesLeft);
         cout << res << endl;
         minValues.clear();
         // nexts.clear();
