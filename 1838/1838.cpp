f92b7614802eff2d7f28357349e937d1388f41af
camargodev <camargo.dev@gmail.com>
2020-09-28 17:21:37 -0300

    Simplified version. Cache still not working

--- 1838/1838.cpp
+++ 1838/1838.cpp
@@ -7,13 +7,14 @@
 using namespace std; 
 
 using num = long long;
+using keyty = pair<num, num>;
 #define loop(n) for (num i=0; i<n; ++i)
 #define loopi(i, n) for (num i=0; i<n; ++i)
 
-#define INF 1000
-
 num TOTAL_MINS = 3600;
 num N;
+
+map<keyty, num> cache;
 
 struct Request {
   num start;
@@ -32,26 +33,32 @@ struct Comp {
 using ReqPQ = priority_queue<Request, vector<Request>, Comp>;
 vector<Request> requests;
 
-num minidle(num currmin, num reqidx, num minsleft, num minsidle, int tabs) {
-    if (currmin > TOTAL_MINS || reqidx == N) { 
+num maxusage(num currmin, num reqidx) {
+    
+    if (currmin == TOTAL_MINS || reqidx == N) { 
         // loop(tabs) cout << " ";
         // cout << "> currmin = " << currmin << " and reqidx = " << reqidx << endl; 
         // loop(tabs) cout << " ";
         // cout << ">>>>> back with " << minsleft << " mins left" << endl; 
-        return minsleft; 
+        return 0; 
     }
 
-    Request req = requests[reqidx];
+    keyty key = make_pair(reqidx, currmin);
+    if (cache.find(key) != cache.end()) 
+        return cache[key];
+   
     // loop(tabs) cout << " ";
     // cout << ">>> Req: " << reqidx << " starts at " << req.start << " - mins left: " << minsleft << endl;
-    if (currmin < req.start) { 
-        // loop(tabs) cout << " ";
-        // cout << "> At minute " << currmin << " but req starts at " << req.start << endl;
-        minsidle += req.start - currmin;
-        // loop(tabs) cout << " ";
-        // cout << "> Idle for " << minsidle << " min " << endl;
-        currmin = req.start;
-    } 
+    // if (currmin < req.start) { 
+    //     // loop(tabs) cout << " ";
+    //     // cout << "> At minute " << currmin << " but req starts at " << req.start << endl;
+    //     minsidle += req.start - currmin;
+    //     // loop(tabs) cout << " ";
+    //     // cout << "> Idle for " << minsidle << " min " << endl;
+    //     currmin = req.start;
+    // } 
+
+
     // else if (req.start < currmin) {
         // loop(tabs) cout << " ";
         // cout << "> Cannot use req, because start already passed at " << currmin << endl;
@@ -59,30 +66,36 @@ num minidle(num currmin, num reqidx, num minsleft, num minsidle, int tabs) {
     // }
     // loop(tabs) cout << " ";
     // cout << "> Not gonna use curr; going to " << reqidx+1 << endl;
-    num minimunidle = minidle(currmin, reqidx+1, minsleft, minsidle, tabs+1);
-    if (minsleft >= req.duration && req.start >= currmin) {
+    Request req = requests[reqidx];
+    num res = maxusage(currmin, reqidx+1);
+    if (currmin <= req.start) {
         // loop(tabs) cout << " ";
         // cout << "> Able to use curr with duration " << req.duration << endl; 
-        minsleft -= req.duration;
+        // minsleft -= req.duration;
         // loop(tabs) cout << " ";
         // cout << "> Available for " << minsleft << " now " << endl;
-        currmin += req.duration;
-        num minimunidlewithcurr = minidle(currmin, reqidx+1, minsleft, minsidle, tabs+1);
-        minimunidle = min(minimunidle, minimunidlewithcurr);
+        // currmin += req.duration;
+
+        num reswithcurr = req.duration + maxusage(req.end, reqidx+1);
+        // reswithcurr += 
+        // num minimunidlewithcurr = minidle(currmin+req.duration, reqidx+1, 
+        //     minsleft-req.duration, minsidle, tabs+1);
+        res = min(res, reswithcurr);
     }
     // loop(tabs) cout << " ";
     // cout << "> I'm req " << reqidx << " returning " << minimunidle << endl;
-    return minimunidle;
-
+    cache[key] = res;
+    return res;
 }
 
 num stoneMaxUse() {
     // cout << "requests = [ ";
     // loopi(i, N) { cout << "( " << requests[i].start << " - " << requests[i].duration << " ) "; }
     // cout << "]" << endl;
-    num idle = minidle(1, 0, TOTAL_MINS, 0, 0);
-    // return idle;
-    return TOTAL_MINS - idle;
+    return maxusage(0, 0);
+    // num idle = minidle(1, 0, TOTAL_MINS, 0, 0);
+    // // return idle;
+    // return TOTAL_MINS - idle;
 }
 
 int main() {
