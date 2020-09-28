6548402a4885f0837ceb7124c8a6a7e2d2255842
camargodev <camargo.dev@gmail.com>
2020-09-28 16:56:24 -0300

    First version fully workin of 1838

--- 1838/1838.cpp
+++ 1838/1838.cpp
@@ -10,6 +10,7 @@ using num = long long;
 #define loop(n) for (num i=0; i<n; ++i)
 #define loopi(i, n) for (num i=0; i<n; ++i)
 
+#define INF 1000
 
 num TOTAL_MINS = 3600;
 num N;
@@ -18,8 +19,8 @@ struct Request {
   num start;
   num duration;
   num end;
-  Request(num start, num duration): 
-    start(start), duration(duration), end(start+duration) {}
+  Request(num start, num end): 
+    start(start), duration(end-start), end(end) {}
 };
 
 struct Comp {
@@ -31,28 +32,58 @@ struct Comp {
 using ReqPQ = priority_queue<Request, vector<Request>, Comp>;
 vector<Request> requests;
 
-num maxUse(num index, num minsleft, num minsidle) {
-    if (index == N) return 0;
-    cout << "at index " << index << endl;
-    Request curr = requests[index];
-    num maxusage = maxUse(index+1, minleft, minsidle);
-    if (minleft >= curr.duration) {
-        cout << " can add curr with duration " << curr.duration << endl;
-        num temp = curr.duration + maxUse(index+1, minleft-curr.duration);
-        cout << "  max with curr = " << temp << endl;
-        cout << "  max without curr = " << maxusage << endl;
-        maxusage = max(maxusage, temp);
+num minidle(num currmin, num reqidx, num minsleft, num minsidle, int tabs) {
+    if (currmin > TOTAL_MINS || reqidx == N) { 
+        // loop(tabs) cout << " ";
+        // cout << "> currmin = " << currmin << " and reqidx = " << reqidx << endl; 
+        // loop(tabs) cout << " ";
+        // cout << ">>>>> back with " << minsleft << " mins left" << endl; 
+        return minsleft; 
     }
-    return maxusage;
+
+    Request req = requests[reqidx];
+    // loop(tabs) cout << " ";
+    // cout << ">>> Req: " << reqidx << " starts at " << req.start << " - mins left: " << minsleft << endl;
+    if (currmin < req.start) { 
+        // loop(tabs) cout << " ";
+        // cout << "> At minute " << currmin << " but req starts at " << req.start << endl;
+        minsidle += req.start - currmin;
+        // loop(tabs) cout << " ";
+        // cout << "> Idle for " << minsidle << " min " << endl;
+        currmin = req.start;
+    } 
+    // else if (req.start < currmin) {
+        // loop(tabs) cout << " ";
+        // cout << "> Cannot use req, because start already passed at " << currmin << endl;
+        // return minsleft;
+    // }
+    // loop(tabs) cout << " ";
+    // cout << "> Not gonna use curr; going to " << reqidx+1 << endl;
+    num minimunidle = minidle(currmin, reqidx+1, minsleft, minsidle, tabs+1);
+    if (minsleft >= req.duration && req.start >= currmin) {
+        // loop(tabs) cout << " ";
+        // cout << "> Able to use curr with duration " << req.duration << endl; 
+        minsleft -= req.duration;
+        // loop(tabs) cout << " ";
+        // cout << "> Available for " << minsleft << " now " << endl;
+        currmin += req.duration;
+        num minimunidlewithcurr = minidle(currmin, reqidx+1, minsleft, minsidle, tabs+1);
+        minimunidle = min(minimunidle, minimunidlewithcurr);
+    }
+    // loop(tabs) cout << " ";
+    // cout << "> I'm req " << reqidx << " returning " << minimunidle << endl;
+    return minimunidle;
+
 }
 
 num stoneMaxUse() {
-    cout << "requests = [ ";
-    loopi(i, N) { cout << requests[i].start << " "; }
-    cout << "]" << endl;
-    return maxUse(0, TOTAL_MINS, TOTAL_MINS);
+    // cout << "requests = [ ";
+    // loopi(i, N) { cout << "( " << requests[i].start << " - " << requests[i].duration << " ) "; }
+    // cout << "]" << endl;
+    num idle = minidle(1, 0, TOTAL_MINS, 0, 0);
+    // return idle;
+    return TOTAL_MINS - idle;
 }
-
 
 int main() {
     cin >> N;
