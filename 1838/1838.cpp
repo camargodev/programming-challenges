aab5d03364c5e06ed7d7d99b4ca2c909217d4628
camargodev <camargo.dev@gmail.com>
2020-09-28 17:31:52 -0300

    1838 working

--- 1838/1838.cpp
+++ 1838/1838.cpp
@@ -11,10 +11,11 @@ using keyty = pair<num, num>;
 #define loop(n) for (num i=0; i<n; ++i)
 #define loopi(i, n) for (num i=0; i<n; ++i)
 
-num TOTAL_MINS = 3600;
+num MAX_MINS = 3600;
 num N;
 
 map<keyty, num> cache;
+// num cache[1001][3601];
 
 struct Request {
   num start;
@@ -33,19 +34,14 @@ struct Comp {
 using ReqPQ = priority_queue<Request, vector<Request>, Comp>;
 vector<Request> requests;
 
-num maxusage(num currmin, num reqidx) {
+num maxUsage(num minute, num index) {
     
-    if (currmin == TOTAL_MINS || reqidx == N) { 
-        // loop(tabs) cout << " ";
-        // cout << "> currmin = " << currmin << " and reqidx = " << reqidx << endl; 
-        // loop(tabs) cout << " ";
-        // cout << ">>>>> back with " << minsleft << " mins left" << endl; 
-        return 0; 
-    }
-
-    keyty key = make_pair(reqidx, currmin);
+    if (index == N) return 0; 
+    if (minute == MAX_MINS) return 0;
+    keyty key = make_pair(index, minute);
     if (cache.find(key) != cache.end()) 
         return cache[key];
+    // if (cache[index][minute] != -1) return cache[index][minute];
    
     // loop(tabs) cout << " ";
     // cout << ">>> Req: " << reqidx << " starts at " << req.start << " - mins left: " << minsleft << endl;
@@ -66,9 +62,9 @@ num maxusage(num currmin, num reqidx) {
     // }
     // loop(tabs) cout << " ";
     // cout << "> Not gonna use curr; going to " << reqidx+1 << endl;
-    Request req = requests[reqidx];
-    num res = maxusage(currmin, reqidx+1);
-    if (currmin <= req.start) {
+    Request request = requests[index];
+    num usage = maxUsage(minute, index+1);
+    if (minute <= request.start) {
         // loop(tabs) cout << " ";
         // cout << "> Able to use curr with duration " << req.duration << endl; 
         // minsleft -= req.duration;
@@ -76,23 +72,23 @@ num maxusage(num currmin, num reqidx) {
         // cout << "> Available for " << minsleft << " now " << endl;
         // currmin += req.duration;
 
-        num reswithcurr = req.duration + maxusage(req.end, reqidx+1);
+        num usageWithCurr = maxUsage(request.end, index+1);
         // reswithcurr += 
         // num minimunidlewithcurr = minidle(currmin+req.duration, reqidx+1, 
         //     minsleft-req.duration, minsidle, tabs+1);
-        res = min(res, reswithcurr);
+        usage = max(usage, usageWithCurr + request.duration);
     }
     // loop(tabs) cout << " ";
     // cout << "> I'm req " << reqidx << " returning " << minimunidle << endl;
-    cache[key] = res;
-    return res;
+    cache[key] = usage;
+    return usage;
 }
 
 num stoneMaxUse() {
     // cout << "requests = [ ";
     // loopi(i, N) { cout << "( " << requests[i].start << " - " << requests[i].duration << " ) "; }
     // cout << "]" << endl;
-    return maxusage(0, 0);
+    return maxUsage(0, 0);
     // num idle = minidle(1, 0, TOTAL_MINS, 0, 0);
     // // return idle;
     // return TOTAL_MINS - idle;
@@ -106,6 +102,7 @@ int main() {
         cin >> start >> duration;
         heap.push(Request(start, duration));
     }
+    // memset(cache, -1, sizeof cache);
     while(heap.size() > 0) {
         Request r = heap.top(); 
         requests.push_back(r);
