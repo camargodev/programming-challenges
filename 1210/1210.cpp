bcaca082e4a1f419697bdb200828c74fa4cc339f
camargodev <camargo.dev@gmail.com>
2020-10-03 11:56:33 -0300

    Changes not on memory

--- 1210/1210.cpp
+++ 1210/1210.cpp
@@ -8,10 +8,10 @@ using namespace std;
 
 using num = int;
 using nums = vector<int>;
-// using key = pair<num, num>;
+using key = pair<num, num>;
 #define loop(i, n) for (num i=0; i<n; ++i)
 struct result {
-  num cost;
+  num cost = 0;
   nums changes;
   result(num cost, nums changes): 
     cost(cost), changes(changes) {}
@@ -20,16 +20,19 @@ struct result {
 
 const num MAXN = 2002;
 num maints[MAXN], prices[MAXN];
+// num dpcost[MAXN][MAXN];
+// nums dpchanges[MAXN][MAXN];
+
 
 num max_year, curr_age, max_age, new_price;
 const num start_age = 1;
 
 // map<key, result> dp;
 
-nums updated_changes(nums changes, num year) {
-    changes.push_back(year+1);
-    return changes;
-}
+// nums updated_changes(nums changes, num year) {
+//     changes.push_back(year+1);
+//     return changes;
+// }
 
 // result build_res(nums ch) {
 //     result res;
@@ -38,53 +41,57 @@ nums updated_changes(nums changes, num year) {
 //     return res;
 // }
 
-result min_cost(num year, num age, nums changes) {
+num min_cost(num year, num age) {
     // const key idx = make_pair(year, age);
 
-    // if (dp.find(idx) != dp.end()) 
-    //     return dp[idx];
-    
-    if (year == max_year) {
-        return result(0, changes);
+    // if (dpcost[year][age] >= 0) 
+    //     return result(dpcost[year][age], dpchanges[year][age]);
+
+    if (year == max_year) return 0;
+    // if (year == max_year) {
+    //     res
+    //     res.changes = changes;
         // result res = build_res(changes);
         // dp[idx] = res;
         // return dp[idx];
         // return res;
-    }
-    num next_year = year+1;
+    // } else {
+    // if (year < max_year) {
 
-    result res = min_cost(next_year, start_age, updated_changes(changes, year));
-    res.cost += (new_price - prices[age-1] + maints[0]);
+    num res = min_cost(year+1, start_age) + (new_price - prices[age-1] + maints[0]);
 
     if (age < max_age) {
-        result temp = min_cost(next_year, age+1, changes);
-        temp.cost += maints[age];
-        if (temp.cost < res.cost) res = temp;
+        num temp = min_cost(year+1, age+1) +  maints[age];
+        res = min(res, temp);
     }
+    // }
 
-    // dp[idx] = res;
+    // dpcost[year][age] = res.cost;
+    // dpchanges[year][age] = res.changes;
     return res;
 }
 
-void print_res(result res) {
-    cout << res.cost << endl;
-    if (res.changes.size() == 0) {
-        cout << 0 << endl;
-    } else {
-        loop(i, res.changes.size())
-            cout << res.changes[i] << " ";
-        cout << endl;
-    }
+void print_res(num res) {
+    cout << res << endl;
+    // if (res.changes.size() == 0) {
+    //     cout << 0 << endl;
+    // } else {
+    //     loop(i, res.changes.size())
+    //         cout << res.changes[i] << " ";
+    //     cout << endl;
+    // }
 }
 
 int main() {
     num start_year = 0;
     while (cin >> max_year) {
-        nums changes;
+        // nums changes;
+        // memset(dpcost, -1, sizeof dpcost);
+        // memset(dpchanges, 0, sizeof dpchanges);
         cin >> curr_age >> max_age >> new_price;
         loop(i, max_age) cin >> maints[i];
         loop(i, max_age) cin >> prices[i];
-        result res = min_cost(start_year, curr_age, changes);
+        num res = min_cost(start_year, curr_age);
         print_res(res);
         // dp.clear();
     }
