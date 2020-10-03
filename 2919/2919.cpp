96828b04b5eddf58754c920aebc8f37acca3bd0c
camargodev <camargo.dev@gmail.com>
2020-10-03 13:23:04 -0300

    Iterative 2919

--- 2919/2919.cpp
+++ 2919/2919.cpp
@@ -15,27 +15,30 @@ num size_vals;
 num vals[MAXN];
 num dp[MAXN];
 
-num max_seq(num idx, num sz, num lastidx) {
-    if (dp[idx] != -1) return dp[idx];
-
-    if (idx == size_vals) return sz;
-
-    num res = max_seq(idx+1, sz, lastidx);
-    if (vals[idx] > vals[lastidx]) {
-        num tmp = max_seq(idx+1, sz+1, idx);
-        res = max(res, tmp);
-    }
-    return dp[idx] = res;
-}
-
 num max_seq_size() {
-    return max_seq(0, 0, -1);
+    dp[0] = -MAXN;
+    num len = 0;
+
+    loop(i, size_vals) {
+        num low = 0, high = len;
+        while (low <= high) {
+            num mid = (low + high)/2;
+            if (dp[mid] < vals[i]) 
+                low = mid+1;
+            else 
+                high = mid-1;
+        }
+        dp[low] = vals[i];
+        if (len < low) len = low;
+    }
+
+    return len;
 }
 
 int main() {
     while(cin >> size_vals) {
         memset(vals, 0, sizeof vals);
-        memset(dp, -1, sizeof dp);
+        memset(dp, MAXN, sizeof dp);
         loop(i, size_vals) cin >> vals[i];
         cout << max_seq_size() << endl;
     }
