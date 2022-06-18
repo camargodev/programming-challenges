da97e575d7690929c8c00d09a705e52986221146
camargodev <camargodev@gmail.com>
2022-06-18 02:53:25 -0300

    Improve truck tour

--- hackerrank/1-week-prep/day-4/mock-test-4.py
+++ hackerrank/1-week-prep/day-4/mock-test-4.py
@@ -1,16 +1,19 @@
-def truckTour(petrol_pumps):
+def truck_tour(petrol_pumps):
     for i in range(len(petrol_pumps)):
         if can_complete_circle(petrol_pumps, i):
             return i
     return None
 
-def can_complete_circle(petrol_pumps, current, cummulated_petrol=0, visited_pumps=0):
+def can_complete_circle(petrol_pumps, current):
+    cummulated_petrol = 0
+    num_of_visited_pumps = 0
     num_of_petrol_pumps = len(petrol_pumps)
-    while visited_pumps < num_of_petrol_pumps:
-        petrol_pump = petrol_pumps[current]
-        if petrol_pump[0] + cummulated_petrol < petrol_pump[1]:
+    while num_of_visited_pumps < num_of_petrol_pumps:
+        pump_available_petrol, distance_to_next = petrol_pumps[current]
+        total_gas = cummulated_petrol + pump_available_petrol
+        if total_gas < distance_to_next:
             return False
-        cummulated_petrol = cummulated_petrol + petrol_pump[0] - petrol_pump[1]
-        visited_pumps += 1
+        cummulated_petrol = total_gas - distance_to_next
+        num_of_visited_pumps += 1
         current = (current+1)%num_of_petrol_pumps
     return True
