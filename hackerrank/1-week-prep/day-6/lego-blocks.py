c7f994f74cf6b98c266ded905c09a9e4f36eb6a3
camargodev <camargodev@gmail.com>
2022-06-18 01:31:16 -0300

    Add 1-WPK day 6

---
+++ hackerrank/1-week-prep/day-6/lego-blocks.py
@@ -0,0 +1,17 @@
+MAX_VAL = 1000000007
+    
+def build_row_formations_by_width(width):
+    formations = [0, 1, 2, 4, 8]
+    for w in range(5, width+1):
+        formations.append((formations[w-1] + formations[w-2] + formations[w-3] + formations[w-4])%MAX_VAL)
+    return formations
+
+def legoBlocks(height, width):
+    num_row_formations = build_row_formations_by_width(width)
+    all_formations = [(num_row**height)%MAX_VAL for num_row in num_row_formations]
+    num_valid_formations = [a for a in all_formations]
+    for i in range(width+1):
+        for j in range(1, i):
+            num_valid_formations[i] -= (num_valid_formations[j]*all_formations[i-j])
+        num_valid_formations[i] = num_valid_formations[i]%MAX_VAL
+    return num_valid_formations[width]
---
+++ hackerrank/1-week-prep/day-6/mock-test-6.py
@@ -0,0 +1,36 @@
+import math
+import os
+import random
+import re
+import sys
+from collections import deque
+
+def build_edges_dict(n, edges):
+    edges_dict = dict()
+    for i in range(n):
+        edges_dict[i+1] = []
+    for edge in edges:
+        node_from = edge[0]
+        node_to = edge[1]
+        edges_dict[node_from].append(node_to)
+        edges_dict[node_to].append(node_from)
+    return edges_dict
+
+def bfs(n, m, edges, s):
+    costs = [-1]*n
+    edges_dict = build_edges_dict(n, edges)
+    visited = set()
+    queue = deque()
+    queue.append((s, 1))
+    visited.add(s)
+    while queue:
+        node, cost = queue.popleft()
+        visited.add(node)
+        for node_to in edges_dict[node]:
+            if node_to in visited:
+                continue
+            costs[node_to-1] = cost*6
+            queue.append((node_to, cost+1))
+            visited.add(node_to)
+            costs.pop(s-1)
+    return costs
