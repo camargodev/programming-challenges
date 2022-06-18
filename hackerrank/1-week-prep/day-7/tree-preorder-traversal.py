7e5c5072fea00eb0b1549cdec7e2d85af19f0fb6
camargodev <camargodev@gmail.com>
2022-06-18 01:34:02 -0300

    Add 1-WPK day 7

---
+++ hackerrank/1-week-prep/day-7/tree-preorder-traversal.py
@@ -0,0 +1,11 @@
+def preOrder(root):
+    values = []
+    pre_order_rec(root, values)
+    print(" ".join(values))
+    
+def pre_order_rec(root, values):
+    if root is None:
+        return
+    values.append(str(root.info))
+    pre_order_rec(root.left, values)
+    pre_order_rec(root.right, values)
