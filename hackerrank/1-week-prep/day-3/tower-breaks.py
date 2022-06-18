e42b8e3a1731a3575130c7147fed3b5449b160cc
camargodev <camargodev@gmail.com>
2022-06-18 02:43:30 -0300

    Improve towers breaks

--- hackerrank/1-week-prep/day-3/tower-breaks.py
+++ hackerrank/1-week-prep/day-3/tower-breaks.py
@@ -1,4 +1,9 @@
-def tower_breakers(n, m):
-    if m == 1:
-        return 2
-    return 2 if n % 2 == 0 else 1
+PLAYER_1_WINS = 1
+PLAYER_2_WINS = 2
+
+def tower_breakers(number_of_towers, tower_height):
+    has_possible_moves = tower_height > 1
+    has_even_num_of_towers = number_of_towers % 2 == 0 
+    if not has_possible_moves:
+        return PLAYER_2_WINS
+    return PLAYER_2_WINS if has_even_num_of_towers else PLAYER_1_WINS
