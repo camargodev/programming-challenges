cf807e8b4b64eca876582759241467795d81eb49
João Vitor de Camargo <camargo.dev@gmail.com>
2022-06-20 10:49:56 -0300

    Update hackerrank/1-week-prep/day-6/lego-blocks.py
    Co-authored-by: Julia Dal Bosco <julia.dalbosco@inf.ufrgs.br>

--- hackerrank/1-week-prep/day-6/lego-blocks.py
+++ hackerrank/1-week-prep/day-6/lego-blocks.py
@@ -6,7 +6,7 @@ def build_row_formations_by_width(width):
         formations.append((formations[w-1] + formations[w-2] + formations[w-3] + formations[w-4])%MAX_VAL)
     return formations
 
-def legoBlocks(height, width):
+def lego_blocks(height, width):
     num_row_formations = build_row_formations_by_width(width)
     all_formations = [(num_row**height)%MAX_VAL for num_row in num_row_formations]
     num_valid_formations = [a for a in all_formations]
