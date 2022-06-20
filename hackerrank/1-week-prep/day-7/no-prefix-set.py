69e7e2d8e1afcbdf00ab038ed64a2ffd92fada69
João Vitor de Camargo <camargo.dev@gmail.com>
2022-06-20 10:46:09 -0300

    Add 1-WPK day 7 (#14)
    * Add 1-WPK day 7
    
    * Update hackerrank/1-week-prep/day-7/tree-preorder-traversal.py
    
    Co-authored-by: Julia Dal Bosco <julia.dalbosco@inf.ufrgs.br>
    
    * Update hackerrank/1-week-prep/day-7/no-prefix-set.py
    
    Co-authored-by: Julia Dal Bosco <julia.dalbosco@inf.ufrgs.br>
    
    * Update hackerrank/1-week-prep/day-7/huffman-decoding.py
    
    Co-authored-by: Julia Dal Bosco <julia.dalbosco@inf.ufrgs.br>
    
    Co-authored-by: Julia Dal Bosco <julia.dalbosco@inf.ufrgs.br>

---
+++ hackerrank/1-week-prep/day-7/no-prefix-set.py
@@ -0,0 +1,20 @@
+def no_prefix(words):
+    previous_words = set()
+    prefixes = set()
+
+    for word in words: 
+        if word in prefixes:
+            print("BAD SET")
+            print(word)
+            return
+        current_sub_word = ""
+        for char in word:
+            current_sub_word += char
+            prefixes.add(current_sub_word)
+            if current_sub_word in previous_words:
+                print("BAD SET")
+                print(word)
+                return
+        previous_words.add(word)
+            
+    print("GOOD SET")
